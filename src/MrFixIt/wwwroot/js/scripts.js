434a59e0a6665d29e1938b84651196a535875d26
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-05-05 09:50:23 -0700

    attempt to change claim functionality to ajax but it is not yet working

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,1 +1,16 @@
-﻿var test = "it works";
+﻿$(document).ready(function () {
+    $('.claim-job').submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/Jobs',
+            dataType: 'json',
+            data: $(this).serialize(),
+            success: function (result) {
+                var claimedJob = result.title;
+                var jobId = result.id.toString();
+                $('#claim-result').text(claimedJob);
+            }
+        });
+    });
+})
