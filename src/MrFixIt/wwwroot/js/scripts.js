91161e2df67985a73ca308d78d1ae144e6c3f114
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-05-05 11:16:54 -0700

    get claim to work in ajax but it doesn't take you back to the jobs list

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,15 +1,19 @@
 ﻿$(document).ready(function () {
     $('.claim-job').submit(function (event) {
         event.preventDefault();
+        console.log("it is reaching the ajax");
+        var newJob = {};
+        var jobProperties = $('.claim-job').serializeArray();
+        newJob.JobId = jobProperties[0].value;
+        newJob.Title = jobProperties[1].value;
+        newJob.Description = jobProperties[2].value;
         $.ajax({
             type: 'POST',
-            url: '/Jobs',
-            dataType: 'json',
-            data: $(this).serialize(),
-            success: function (result) {
-                var claimedJob = result.title;
-                var jobId = result.id.toString();
-                $('#claim-result').text(claimedJob);
+            url: $(this).data('url-action'),
+            datatype: 'json',
+            data: newJob,
+            success: function (success) {
+                console.log("it worked");
             }
         });
     });
